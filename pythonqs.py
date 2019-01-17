11be669781c11a2b10010d62a5419f56c1f4f6d3
David Martin <d.m.a.martin@dundee.ac.uk>
2019-01-17 16:51:10 +0000

    refactor to csv output

--- pythonqs.py
+++ pythonqs.py
@@ -5,7 +5,7 @@ Created on Tue Jan 15 08:35:13 2019
 @author: David
 """
 
-import random
+import random,csv
 
 KEYWORDS = """False       class        finally     is               return
 None       continue   for          lambda      try
@@ -20,102 +20,86 @@ NOTKEYWORDS = "finish length elseif delete remove last module array dictionary w
 RANDOMWORDS = [x.strip() for x in open('words_alpha.txt','r').readlines()]
 
 def arrayq1(qid):
+    question = {'qtype': 'MC', 'correct': [], 'incorrect': [], 'prompt': ''}
     arr = get_random_array()
     index = random.randint(0,len(arr)-1) 
     text = """Given the array arr = {}, give the code which will retrieve the element with value {}"""
-    qtext = '''Q: Array Question {qid}
-T: MC
-D: {text}
-C+: {correct}
-'''.format(qid=qid, text=text.format(str(arr), arr[index]), correct="arr[{}]".format(index))
-    qtext += "C: arr({})\n".format(index)
-    qtext += "C: arr{{{}}}\n".format(index+1)
-    qtext += "C: arr({})\n".format(index+1)
-    qtext += "C: arr[{}]\n".format(index+1)
-    return qtext
+    question['prompt'] = text.format(str(arr), arr[index])
+    question['correct'].append("arr[{}]".format(index))
+    question['incorrect'].append( "arr({})".format(index))
+    question['incorrect'].append( "arr{{{}}}".format(index+1))
+    question['incorrect'].append( "arr({})".format(index+1))
+    question['incorrect'].append( "arr[{}]".format(index+1))
+    return question
 
 def arrayq2(qid):
+    question = {'qtype': 'MC', 'correct': [], 'incorrect': [], 'prompt': ''}
     arr = get_random_array()
     index = random.randint(1,len(arr)-1) 
     text = """Given the array arr = {}, give the value which will be returned with the code {}"""
-    qtext = '''Q: Array Question {qid}
-T: MC
-D: {text}
-C+: {correct}
-'''.format(qid=qid, text=text.format(str(arr), "arr[{}]".format(index)), correct=arr[index])
-    qtext += "C: {}\n".format(arr[index+1])
-    qtext += "C: {}\n".format(arr[index-1])
-    qtext += "C: An error occurs\n"
-    return qtext
+    question['prompt'] = text.format(str(arr), "arr[{}]".format(index))
+    question['correct'].append(arr[index])
+    question['incorrect'].append( arr[index+1])
+    question['incorrect'].append( arr[index-1])
+    question['incorrect'].append( "An error occurs")
+    return question
 
 def arrayq3(qid):    
+    question = {'qtype': 'MC', 'correct': [], 'incorrect': [], 'prompt': ''}
     arr = get_random_array()
     index = random.randint(1,len(arr)-1) 
-    text = """Given the array arr = {}, give the value which will be returned with the code {}"""
     incorrect = ("arr({})", "arr{{{}}}")
-    qtext = '''Q: Array Question {qid}
-T: MC
-D: {text}
-C+: {correct}
-'''.format(qid=qid, text=text.format(str(arr), random.choice(incorrect).format(index)), 
-    correct="An error occurs")
-    qtext += "C: {}\n".format(arr[index+1])
-    qtext += "C: {}\n".format(arr[index-1])
-    qtext += "C: {}\n".format(arr[index])
-    return qtext
+    text = """Given the array arr = {}, give the value which will be returned with the code {}"""
+    question['prompt'] = text.format(str(arr), random.choice(incorrect).format(index))
+    question['correct'].append("An error occurs")
+    question['incorrect'].append(arr[index+1])
+    question['incorrect'].append(arr[index])
+    question['incorrect'].append(arr[index-1])
+
+    return question
     
 def arrayq4(qid):
+    question = {'qtype': 'MC', 'correct': [], 'incorrect': [], 'prompt': ''}
     arr = get_random_array()
     startindex = random.randint(1,len(arr)-int(len(arr)/3))
     endindex = random.randint(startindex+1, len(arr)-1)
     text = """Given the array arr = {}, give the values which will be returned with the code {}"""
-    qtext = '''Q: Array Question {qid}
-T: MC
-D: {text}
-C+: {correct}
-'''.format(qid=qid, text=text.format(str(arr), "arr[{}:{}]".format(startindex, endindex)), 
-    correct= arr[startindex:endindex])
-    qtext += "C: {}\n".format(arr[startindex:endindex+1])
-    qtext += "C: {}\n".format(arr[startindex-1:endindex])
-    qtext += "C: {}\n".format(arr[startindex-1:endindex-1])
-    qtext += "C: An error occurs\n"
-    return qtext
+    question['prompt'] = text.format(str(arr), "arr[{}:{}]".format(startindex, endindex))
+    question['correct'].append(arr[startindex:endindex])
+    question['incorrect'].append(arr[startindex:endindex+1])
+    question['incorrect'].append(arr[startindex-1:endindex])
+    question['incorrect'].append(arr[startindex-1:endindex-1])
+    return question
 
 def arrayq5(qid):
+    question = {'qtype': 'MC', 'correct': [], 'incorrect': [], 'prompt': ''}
     arr = get_random_array()
     startindex = random.randint(1,len(arr)-int(len(arr)/3))
     endindex = random.randint(startindex+1, len(arr)-1)
     text = """Given the array arr = {}, give the code which will return the values {}"""
-    qtext = '''Q: Array Question {qid}
-T: MC
-D: {text}
-C+: {correct}
-'''.format(qid=qid, text=text.format(str(arr), arr[startindex:endindex]), 
-    correct= "arr[{}:{}]".format(startindex,endindex))
-    qtext += "C: arr[{}:{}]\n".format(startindex,endindex-1)
-    qtext += "C: arr[{}:{}]\n".format(startindex+1,endindex)
-    qtext += "C: arr[{}:{}]\n".format(startindex+1,endindex+1)
-    qtext += "C: An error occurs\n"
-    return qtext
+    question['prompt'] = text.format(str(arr), arr[startindex:endindex])
+    question['correct'].append("arr[{}:{}]".format(startindex,endindex))
+    question['incorrect'].append("arr[{}:{}]".format(startindex,endindex-1))
+    question['incorrect'].append("arr[{}:{}]".format(startindex+1,endindex))
+    question['incorrect'].append("An error occurs")
+    return question
 
 def arrayq6(qid):
+    question = {'qtype': 'MC', 'correct': [], 'incorrect': [], 'prompt': ''}
     arr = get_random_array()
     startindex = random.randint(1,len(arr)-int(len(arr)/3))
     endindex = random.randint(startindex+1, len(arr)-1)
     text = """Given the array arr = {}, give the code which will return the values {}"""
-    qtext = '''Q: Array Question {qid}
-T: MC
-D: {text}
-C+: {correct}
-'''.format(qid=qid, text=text.format(str(arr), arr[startindex:endindex]), 
-    correct= "arr[{}:{}]".format(startindex,endindex))
-    qtext += "C: arr[{},{}]\n".format(startindex,endindex)
-    qtext += "C: arr[{}-{}]\n".format(startindex,endindex)
-    qtext += "C: arr({}:{})\n".format(startindex,endindex)
-    qtext += "C: An error occurs\n"
-    return qtext
+    question['prompt'] = text.format(str(arr), arr[startindex:endindex])
+    question['correct'].append("arr[{}:{}]".format(startindex,endindex))
+    question['incorrect'].append("arr[{},{}]".format(startindex,endindex))
+    question['incorrect'].append("arr[{}-{}]".format(startindex,endindex))
+    question['incorrect'].append("arr({}:{})".format(startindex,endindex))
+    question['incorrect'].append("An error occurs")
+    return question
 
 def varq7(qid):
+    question = {'qtype': 'MC', 'correct': [], 'incorrect': [], 'prompt': ''}
     var = random.random()*random.randint(1,10)
     options=(str, int, float)
     var = random.choice(options)(round(var,0))
@@ -125,20 +109,17 @@ def varq7(qid):
     <code>&gt; var
     {}
     </code>]]>"""
-    qtext = '''Q: Variable Question {qid}
-T: MC
-D: {text}
-C+: {correct}
-'''.format(qid=qid, text=text.format(var), 
-    correct= type(var).__name__)
+    question['prompt'] = text.format(var)
+    question['correct'].append(type(var).__name__)
     opts = ['str','int','float']
     opts.remove(type(var).__name__)
     for t in opts:
-        qtext += "C: {}\n".format(t)
-    qtext += "C: Something else\n"
-    return qtext
+        question['incorrect'].append("{}".format(t))
+    question['incorrect'].append("Something else")
+    return question
 
 def varq8(qid):
+    question = {'qtype': 'MC', 'correct': [], 'incorrect': [], 'prompt': ''}
     
     var1 = random.random()*random.randint(1,10)
     var2 = random.random()*random.randint(1,10)
@@ -159,42 +140,36 @@ def varq8(qid):
     &gt; b = {}
     &gt; c = a + b
     </code>]]>"""
-    qtext = '''Q: Variable Question {qid}
-T: MC
-D: {text}
-C+: {correct}
-'''.format(qid=qid, text=text.format(var1,var2), 
-    correct=correct)
+    question['prompt'] = text.format(var1,var2)
+    question['correct'].append(correct)
     opts = ['str','int','float']
     if correct in opts:
         opts.remove(correct)
-        qtext += "C: An error occurs\n"
+        question['incorrect'].append("An error occurs")
     for a in opts:
-        qtext += "C: {}\n".format(a)
-    return qtext
+        question['incorrect'].append(a)
+    return question
 
 def varq9(qid):
-    qtext = '''Q: Variable Question {qid}
-T: MR
-D: Which of the following are legitimate variable names? Select all that apply.
-'''
-    qtext += "C: {}\n".format(random.choice(KEYWORDS))
-    qtext += "C+: {}\n".format(random.choice(NOTKEYWORDS))
+    question = {'qtype': 'MR', 'correct': [], 'incorrect': [], 'prompt': ''}
+    question['prompt'] ="Which of the following are legitimate variable names? Select all that apply."
+    question['incorrect'].append(random.choice(KEYWORDS))
+    question['correct'].append(random.choice(NOTKEYWORDS))
     word = random.choice(RANDOMWORDS)
     if word in KEYWORDS:
-        qtext += "C: {}\n".format(word)
+        question['incorrect'].append(word)
     else:
-        qtext += "C+: {}\n".format(word)
+        question['correct'].append(word)
     word = random.choice(RANDOMWORDS)
-    qtext += "C: {}\n".format(word + random.choice([" ",".","-"])+str(random.randint(0,9)))
+    question['incorrect'].append(word + random.choice([" ",".","-"])+str(random.randint(0,9)))
     words = random.choices(RANDOMWORDS, k=random.randint(2,5))
-    qtext += "C: {}\n".format(random.choice([" ","-","."]).join([x.capitalize() for x in words]))
+    question['incorrect'].append(random.choice([" ","-","."]).join([x.capitalize() for x in words]))
     words = random.choices(RANDOMWORDS, k=random.randint(2,5))
-    qtext += "C+: {}\n".format(random.choice(["","_"]).join([x.capitalize() for x in words]))
-    
-    return qtext
+    question['correct'].append(random.choice(["","_"]).join([x.capitalize() for x in words]))
+    return question
 
 def dictq10(qid):
+    question = {'qtype': 'MC', 'correct': [], 'incorrect': [], 'prompt': ''}
     count = random.randint(3,7)
     words = random.choices(RANDOMWORDS, k=count)
     numbers = []
@@ -203,17 +178,15 @@ def dictq10(qid):
     d = dict(zip(words,numbers))
     keypairs = ", ".join(["'{}' &rarr; {}".format(k,v) for k,v in d.items()])
 
-    qtext = '''Q: Dictionary Question {qid}
-T: MC
-D: <![CDATA[Which of the following is the correct way to define the dictionary <tt>d</tt> for the key &rarr; value pairs {kv} ?.
-]]>'''.format(qid=qid, kv=keypairs)
-    qtext += "C+: d = {}\n".format(str(d))
-    qtext += "C: d = [{}]\n".format(list(d.items()))
-    qtext += "C: d = {{{}}}\n".format(", ".join(["{} : {}".format(k,v) for k,v in d.items()]))
-    qtext += "C: d = {{{}}}\n".format(list(d.items()))
-    return qtext
+    question['prompt'] = "<![CDATA[Which of the following is the correct way to define the dictionary <tt>d</tt> for the key &rarr; value pairs {kv} ?.]]>".format( kv=keypairs)
+    question['correct'].append("d = {}".format(str(d)))
+    question['incorrect'].append("d = {}".format(list(d.items())))
+    question['incorrect'].append("d = {{{}}}".format(", ".join(["{} : {}".format(k,v) for k,v in d.items()])))
+    question['incorrect'].append( "d = {{{}}}".format(list(d.items())))
+    return question
 
 def dictq11(qid):
+    question = {'qtype': 'MC', 'correct': [], 'incorrect': [], 'prompt': ''}
     count = random.randint(3,7)
     words = random.choices(RANDOMWORDS, k=count)
     numbers = []
@@ -222,22 +195,19 @@ def dictq11(qid):
         numbers.append(random.randint(1,100))
     d = dict(zip(words,numbers))
     keypairs = ", ".join(["'{}' &rarr; {}".format(k,v) for k,v in d.items()])
-    qtext = '''Q: Dictionary Question {qid}
-T: MC
-D: <![CDATA[Which of the following is the correct way to retrieve the value indexed by <tt>{key}</tt> from the dictionary <tt>d</tt> containing the following key &rarr; value pairs: {kv}?  
-]]>'''.format(qid=qid, key=randkey, kv=keypairs)
-    qtext += "C+: d['{}']\n".format(randkey)
-    qtext += "C: d[{}]\n".format(randkey)
-    qtext += "C: d{{{}}}\n".format(randkey)
-    qtext += "C: d{{'{}'}}\n".format(randkey)
-    qtext += "C: d({})\n".format(randkey)
-    qtext += "C: d<{}>\n".format(randkey)
-    return qtext
+    question['prompt'] = '<![CDATA[Which of the following is the correct way to retrieve the value indexed by <tt>{key}</tt> from the dictionary <tt>d</tt> containing the following key &rarr; value pairs: {kv}?  ]]>'.format( key=randkey, kv=keypairs)
+    question['correct'].append("d['{}']".format(randkey))
+    question['incorrect'].append("d[{}]".format(randkey))
+    question['incorrect'].append("d{{{}}}".format(randkey))
+    question['incorrect'].append("d{{'{}'}}".format(randkey))
+    question['incorrect'].append("d({})".format(randkey))
+    question['incorrect'].append("d<{}>".format(randkey))
+    return question
 
 def listq12(qid):
+    question = {'qtype': 'MC', 'correct': [], 'incorrect': [], 'prompt': ''}
     dim1 = random.randint(3,5)
     dim2 = random.randint(2,4)
-    
     arr=[]
     for p in range(dim1):
         arr.append( [round(random.random()*random.randint(1,10),2) for __ in range(dim2)])
@@ -245,22 +215,20 @@ def listq12(qid):
     qdim2 = qdim1
     while qdim2 == qdim1:
         qdim2 =random.randint(0,dim2-1)
-    qtext = '''Q: Dictionary Question {qid}
-T: MC
-D: <![CDATA[Which of the following is the correct way to retrieve the value {val} from the list <tt>l</tt> {lv}?  
-]]>'''.format(qid=qid, val=arr[qdim1][qdim2], lv=str(arr))
-    qtext += "C+: l[{d1}][{d2}]\n".format(d1=qdim1, d2=qdim2)
-    qtext += "C: l[{d1},{d2}]\n".format(d1=qdim1, d2=qdim2)
-    qtext += "C: l[{d1}:{d2}]\n".format(d1=qdim1, d2=qdim2)
-    qtext += "C: l[{d1}][{d2}]\n".format(d1=qdim1+1, d2=qdim2+1)
-    qtext += "C: l[{d1},{d2}]\n".format(d1=qdim1+1, d2=qdim2+1)
-    qtext += "C: l[{d1}:{d2}]\n".format(d1=qdim1+1, d2=qdim2+1)
-    return qtext
+    question['prompt'] = '<![CDATA[Which of the following is the correct way to retrieve the value {val} from the list <tt>l</tt> {lv}?  ]]>'.format( val=arr[qdim1][qdim2], lv=str(arr))
+    question['correct'].append("l[{d1}][{d2}]".format(d1=qdim1, d2=qdim2))
+    question['incorrect'].append("l[{d1},{d2}]".format(d1=qdim1, d2=qdim2))
+    question['incorrect'].append("l[{d1}:{d2}]".format(d1=qdim1, d2=qdim2))
+    question['incorrect'].append("l[{d1}][{d2}]".format(d1=qdim1+1, d2=qdim2+1))
+    question['incorrect'].append("l[{d1},{d2}]".format(d1=qdim1+1, d2=qdim2+1))
+    question['incorrect'].append("l[{d1}:{d2}]".format(d1=qdim1+1, d2=qdim2+1))
+    return question
     
 def listq13(qid):
+    question = {'qtype': 'MC', 'correct': [], 'incorrect': [], 'prompt': ''}
     dim1 = random.randint(3,5)
     dim2 = random.randint(3,4)
-    
+    print(qid,":")
     arr=[]
     for p in range(dim1):
         arr.append( [round(random.random()*random.randint(1,10),2) for __ in range(dim2)])
@@ -268,23 +236,28 @@ def listq13(qid):
     qdim2 = qdim1
     while qdim2 == qdim1:
         qdim2 =random.randint(0,min(dim1,dim2)-2)
-    qtext = '''Q: Dictionary Question {qid}
-T: MC
-D: <![CDATA[Which of the following is the value returned by <tt>{val}</tt> from the list <code>l =  {lv}</code>?  
-]]>'''.format(qid=qid, val="arr[{}][{}]".format(qdim1,qdim2), lv=str(arr))
-    qtext += "C+: {}\n".format(arr[qdim1][qdim2])
-    qtext += "C: {}\n".format(arr[qdim1+1][qdim2+1])
-    qtext += "C: {}\n".format(arr[qdim2][qdim1])
-    qtext += "C: {}\n".format(arr[qdim2+1][qdim1+1])
-    qtext += "C: An error occurs\n"
-    return qtext
+    question['prompt'] = '<![CDATA[Which of the following is the value returned by <tt>{val}</tt> from the list <code>l =  {lv}</code>?  ]]>'.format( val=arr[qdim1][qdim2], lv=str(arr))
+    question['correct'].append("{}".format(arr[qdim1][qdim2]))
+    question['incorrect'].append("{}".format(arr[qdim1+1][qdim2+1]))
+    question['incorrect'].append("{}".format(arr[qdim2][qdim1]))
+    question['incorrect'].append("{}".format(arr[qdim2+1][qdim1+1]))
+    question['incorrect'].append("An error occurs")
+    return question
     
-        
 
+
+        
 def get_random_array(lenfrom=5, lento=10):   
     arr = []
     for p in range(random.randint(lenfrom, lento)):
         arr.append(random.random()*random.randint(1,10))
     arr = [round(x, 2) for x in set(arr)]
     return arr
-        
+
+def qtoCSV(q,csvfh):
+    line=[q.get('qtype', 'MC'), q.get('prompt', 'No Question')]
+    for c in q['correct']:
+        line += [c, 1, '']
+    for i in q['incorrect']:
+        line += [i, 0, '']
+    csvfh.writerow(line)
